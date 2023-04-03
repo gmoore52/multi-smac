d7156f2456938d040dcd03c610ef6fd025ba9b1e
Gavin Moore <33696395+gmoore52@users.noreply.github.com>
2023-04-03 08:41:45 -0500

    Update __init__.py

--- smac/env/__init__.py
+++ smac/env/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from smac.env.multiagentenv import MultiAgentEnv
+from smac.env.multiagentenv import MultiSidedMultiAgentEnv
 from smac.env.starcraft2.starcraft2 import StarCraft2Env
 
 __all__ = ["MultiAgentEnv", "StarCraft2Env"]
